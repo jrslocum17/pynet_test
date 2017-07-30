22e4012ee42089ec4f0eac1a3d276461a491cbf6
Justin Slocum <justin.slocum@gmail.com>
2017-07-30 15:12:23 -0700

    adding create_list.py, read_and_print_list.py, and assorted json and yaml files

---
+++ create_list.py
@@ -0,0 +1,21 @@
+#!/home/jslocum/VENV/py27_venv/bin/python
+
+
+import yaml
+import json
+from pprint import pprint as pp
+
+some_list = range(4)
+some_list.append("hello")
+some_list.append("goodbye")
+some_list.append({})
+some_list[-1]['mammal'] = "bear"
+some_list[-1]['amphibian'] = "frog"
+
+pp(some_list)
+
+with open("list_output.json", "w") as f:
+ json.dump(some_list,f)
+
+with open("list_output.yml", "w") as f:
+ f.write(yaml.dump(some_list,default_flow_style=False))
---
+++ read_and_print_list.py
@@ -0,0 +1,20 @@
+#!/home/jslocum/VENV/py27_venv/bin/python
+
+
+import yaml
+import json
+
+from pprint import pprint as pp
+
+with open("list_output.yml") as f:
+ yaml_list=yaml.load(f)
+
+with open("list_output.json") as f:
+ json_list=json.load(f)
+
+print("YAML List:")
+pp(yaml_list)
+
+print("")
+print("JSON List:")
+pp(json_list)
